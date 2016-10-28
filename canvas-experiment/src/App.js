43a969304d03b52a8eaaed3af56a9429e1f7113b
Christopher Bartling <chris.bartling@gmail.com>
2016-10-27 23:16:04 -0500

    Image viewer now working properly.

--- canvas-experiment/src/App.js
+++ canvas-experiment/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ImageViewer from "./components/image-viewer/ImageViewer";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Canvas experiment</h2>
+                </div>
+                <ImageViewer width={800} height={600}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ canvas-experiment/src/components/image-viewer/ImageViewer.js
@@ -0,0 +1,116 @@
+// @flow
+
+import React, {Component} from "react";
+import * as ReactDOM from "react-dom";
+import testImage from "./leaves.jpg";
+import "./ImageViewer.css";
+import trackTransforms from "./CanvasContextTrackTransforms";
+
+class ImageViewer extends Component {
+
+    static propTypes = {
+        height: React.PropTypes.number.isRequired,
+        width: React.PropTypes.number.isRequired
+    };
+
+    constructor(props, context) {
+        super(props, context);
+        this.image = new Image();
+        this.lastX = undefined;
+        this.lastY = undefined;
+        this.dragStart = undefined;
+        this.dragged = false;
+        this.scaleFactor = 1.2;
+        this.onMouseDownCanvas = this.onMouseDownCanvas.bind(this);
+        this.onMouseMoveCanvas = this.onMouseMoveCanvas.bind(this);
+        this.onMouseUpCanvas = this.onMouseUpCanvas.bind(this);
+        this.onMouseWheelCanvas = this.onMouseWheelCanvas.bind(this);
+        this.onLoadImage = this.onLoadImage.bind(this);
+        this.image.addEventListener('load', this.onLoadImage, false);
+        this.image.src = testImage;
+    };
+
+    onLoadImage(evt) {
+        this.redraw();
+    }
+
+    redraw() {
+        if (this.ctx && this.canvas && this.image) {
+            let p1 = this.ctx.transformedPoint(0, 0);
+            let p2 = this.ctx.transformedPoint(this.canvas.width, this.canvas.height);
+            this.ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+            this.ctx.save();
+            this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.restore();
+            this.ctx.drawImage(this.image, 0, 0);
+        }
+    };
+
+    zoom(clicks) {
+        let pt = this.ctx.transformedPoint(this.lastX, this.lastY);
+        this.ctx.translate(pt.x, pt.y);
+        let factor = Math.pow(this.scaleFactor, clicks);
+        this.ctx.scale(factor, factor);
+        this.ctx.translate(-pt.x, -pt.y);
+        this.redraw();
+    };
+
+    onMouseDownCanvas(evt) {
+        document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+        this.lastX = evt.offsetX || (evt.pageX - this.canvas.offsetLeft);
+        this.lastY = evt.offsetY || (evt.pageY - this.canvas.offsetTop);
+        this.dragStart = this.ctx.transformedPoint(this.lastX, this.lastY);
+        this.dragged = false;
+    };
+
+    onMouseMoveCanvas(evt) {
+        this.lastX = evt.offsetX || (evt.pageX - this.canvas.offsetLeft);
+        this.lastY = evt.offsetY || (evt.pageY - this.canvas.offsetTop);
+        this.dragged = true;
+        if (this.dragStart) {
+            let pt = this.ctx.transformedPoint(this.lastX, this.lastY);
+            this.ctx.translate(pt.x - this.dragStart.x, pt.y - this.dragStart.y);
+            this.redraw();
+        }
+    };
+
+    onMouseUpCanvas(evt) {
+        this.dragStart = null;
+        if (!this.dragged) this.zoom(evt.shiftKey ? -1 : 1);
+    };
+
+    onMouseWheelCanvas(evt) {
+        let delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
+        if (delta) this.zoom(delta);
+        return evt.preventDefault() && false;
+    };
+
+    componentDidMount() {
+        this.canvas = ReactDOM.findDOMNode(this);
+        this.ctx = this.canvas.getContext('2d');
+        trackTransforms(this.ctx);
+        this.lastX = this.canvas.width / 2;
+        this.lastY = this.canvas.height / 2;
+        this.canvas.addEventListener('mousedown', this.onMouseDownCanvas, false);
+        this.canvas.addEventListener('mousemove', this.onMouseMoveCanvas, false);
+        this.canvas.addEventListener('mouseup', this.onMouseUpCanvas, false);
+        this.canvas.addEventListener('DOMMouseScroll', this.onMouseWheelCanvas, false);
+        this.canvas.addEventListener('mousewheel', this.onMouseWheelCanvas, false);
+    };
+
+    componentWillUnmount() {
+    };
+
+    render() {
+        return (
+            <canvas className="image-viewer-canvas"
+                    height={this.props.height}
+                    width={this.props.width}/>
+        );
+    }
+}
+
+export default ImageViewer;
+
+
