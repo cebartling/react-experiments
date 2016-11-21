5ced1d12ba33e7f0ec8dda9117d50e9ffff72faf
Christopher Bartling <chris.bartling@gmail.com>
2016-11-21 15:26:40 -0600

    Hooked up GSAP-based animation.

--- css-transition-group/src/App.js
+++ css-transition-group/src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+import ReactTransitionGroup from "react-addons-transition-group";
+import Box from "./Box";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            shouldShowBox: true
+        };
+    }
+
+    toggleBox = () => {
+        this.setState({
+            shouldShowBox: !this.state.shouldShowBox
+        });
+    };
+
+    render() {
+        return <div className="page">
+
+            <ReactTransitionGroup>
+                { this.state.shouldShowBox && <Box/>}
+            </ReactTransitionGroup>
+
+            <button
+                className="toggle-btn"
+                onClick={this.toggleBox}> Toggle
+            </button>
+        </div>;
+    }
+
 }
 
 export default App;
---
+++ css-transition-group/src/ChildView.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ChildView extends Component {
+
+    render() {
+        return (
+            <div className="child-view">
+                <p>A transition example</p>
+            </div>
+        );
+    }
+}
+
+export default ChildView;
