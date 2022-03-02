d4d8c587439bd480ce605eeaf6a8f22bc6eb8cc4
Christopher Bartling <chris.bartling@gmail.com>
2022-03-02 16:02:32 -0600

    Removing unneeded Storybook stuff

--- charting-experiments/src/stories/Page.tsx
+++ charting-experiments/src/stories/Page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { Header } from './Header';
 import './page.css';
 
 type User = {
@@ -8,17 +7,9 @@ type User = {
 };
 
 export const Page: React.VFC = () => {
-  const [user, setUser] = React.useState<User>();
 
   return (
     <article>
-      <Header
-        user={user}
-        onLogin={() => setUser({ name: 'Jane Doe' })}
-        onLogout={() => setUser(undefined)}
-        onCreateAccount={() => setUser({ name: 'Jane Doe' })}
-      />
-
       <section>
         <h2>Pages in Storybook</h2>
         <p>
