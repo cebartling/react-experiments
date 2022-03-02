ce3f85f1252097a1ae6eae838c4f7690a9fa158b
Christopher Bartling <chris.bartling@gmail.com>
2022-03-02 16:48:55 -0600

    Added PieChart

---
+++ charting-experiments/src/stories/components/PieChart.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import SimpleChart from "../../components/SimpleChart";
+import PieChart from "../../components/PieChart";
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Charts/Pie',
+  component: PieChart,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    // backgroundColor: {control: 'color'},
+  },
+} as ComponentMeta<typeof PieChart>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// @ts-ignore
+const Template: ComponentStory<typeof PieChart> = (args) => <PieChart {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  // primary: true,
+  // label: 'Button',
+};
+
