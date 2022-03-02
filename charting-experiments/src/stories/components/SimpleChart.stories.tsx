e98d3fca755fcce370fb5352479ff0156747dc87
Christopher Bartling <chris.bartling@gmail.com>
2022-03-02 15:26:14 -0600

    Built out SimpleChart component and story

---
+++ charting-experiments/src/stories/components/SimpleChart.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import SimpleChart from "../../components/SimpleChart";
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Charts/Simple',
+  component: SimpleChart,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    // backgroundColor: {control: 'color'},
+  },
+} as ComponentMeta<typeof SimpleChart>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// @ts-ignore
+const Template: ComponentStory<typeof SimpleChart> = (args) => <SimpleChart {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  // primary: true,
+  // label: 'Button',
+};
+
