b0ece9a332e1d2302d9ba960583bb0f92f2fb721
Christopher Bartling <chris.bartling@gmail.com>
2022-03-02 16:10:37 -0600

    Added AnotherSimpleChart

---
+++ charting-experiments/src/components/AnotherSimpleChart.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import ReactECharts from 'echarts-for-react';
+
+const AnotherSimpleChart: React.FC = () => {
+  const option = {
+    title: {
+      text: 'Another Simple Chart Title'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data: ['Rocks', 'Paper', 'Scissors']
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: ['one', 'two', 'three', 'four', 'five', 'six', 'sever']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: 'Series 1',
+        type: 'line',
+        stack: 'stack',
+        areaStyle: {normal: {}},
+        data: [120, 132, 101, 134, 90, 230, 210]
+      },
+      {
+        name: 'Series 2',
+        type: 'line',
+        stack: 'stack',
+        areaStyle: {normal: {}},
+        data: [220, 182, 191, 234, 290, 330, 310]
+      },
+      {
+        name: 'Series 3',
+        type: 'line',
+        stack: 'stack',
+        areaStyle: {normal: {}},
+        data: [150, 232, 201, 154, 190, 330, 410]
+      }
+    ]
+  };
+
+  return <ReactECharts
+    option={option}
+    style={{height: 400}}
+  />;
+};
+
+export default AnotherSimpleChart;
---
+++ charting-experiments/src/stories/components/AnotherSimpleChart.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import SimpleChart from "../../components/SimpleChart";
+import AnotherSimpleChart from "../../components/AnotherSimpleChart";
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Charts/Another Simple',
+  component: AnotherSimpleChart,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    // backgroundColor: {control: 'color'},
+  },
+} as ComponentMeta<typeof AnotherSimpleChart>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// @ts-ignore
+const Template: ComponentStory<typeof AnotherSimpleChart> = (args) => <AnotherSimpleChart {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  // primary: true,
+  // label: 'Button',
+};
+
