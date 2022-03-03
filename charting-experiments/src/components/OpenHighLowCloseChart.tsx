43c654afe453dccdecfcc276a2f0dfed40f445c2
Christopher Bartling <chris.bartling@gmail.com>
2022-03-02 21:10:21 -0600

    Added OpenHighLowCloseChart

---
+++ charting-experiments/src/components/OpenHighLowCloseChart.tsx
@@ -0,0 +1,164 @@
+import React from 'react';
+import ReactECharts from 'echarts-for-react';
+import {CustomSeriesRenderItemAPI, CustomSeriesRenderItemParams, CustomSeriesRenderItemReturn} from "echarts";
+
+function renderItem(
+  params: CustomSeriesRenderItemParams,
+  api: CustomSeriesRenderItemAPI
+): CustomSeriesRenderItemReturn {
+  const xValue = api.value(0);
+  const openPoint = api.coord([xValue, api.value(1)]);
+  const closePoint = api.coord([xValue, api.value(2)]);
+  const lowPoint = api.coord([xValue, api.value(3)]);
+  const highPoint = api.coord([xValue, api.value(4)]);
+  const halfWidth = (api.size!([1, 0]) as number[])[0] * 0.35;
+  const style = api.style({
+    stroke: api.visual('color')
+  });
+
+  return {
+    type: 'group',
+    children: [
+      {
+        type: 'line',
+        shape: {
+          x1: lowPoint[0],
+          y1: lowPoint[1],
+          x2: highPoint[0],
+          y2: highPoint[1]
+        },
+        style: style
+      },
+      {
+        type: 'line',
+        shape: {
+          x1: openPoint[0],
+          y1: openPoint[1],
+          x2: openPoint[0] - halfWidth,
+          y2: openPoint[1]
+        },
+        style: style
+      },
+      {
+        type: 'line',
+        shape: {
+          x1: closePoint[0],
+          y1: closePoint[1],
+          x2: closePoint[0] + halfWidth,
+          y2: closePoint[1]
+        },
+        style: style
+      }
+    ]
+  };
+}
+
+export type OpenHighLowCloseChartData = {
+  categoryData: any;
+  values: any;
+};
+
+const OpenHighLowCloseChart = ({categoryData, values}: OpenHighLowCloseChartData) => {
+
+  const option = {
+    animation: false,
+    legend: {
+      bottom: 10,
+      left: 'center',
+      data: ['Dow-Jones index']
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross'
+      },
+      position: function (pos: any[], params: any, el: any, elRect: any, size: any) {
+        var obj: Record<string, number> = {top: 10};
+        obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 30;
+        return obj;
+      }
+    },
+    axisPointer: {
+      link: [{xAxisIndex: 'all'}]
+    },
+    toolbox: {
+      feature: {
+        dataZoom: {
+          yAxisIndex: false
+        },
+        brush: {
+          type: ['lineX', 'clear']
+        }
+      }
+    },
+    grid: [
+      {
+        left: '10%',
+        right: '8%',
+        bottom: 150
+      }
+    ],
+    xAxis: [
+      {
+        type: 'category',
+        data: categoryData,
+        boundaryGap: false,
+        axisLine: {onZero: false},
+        splitLine: {show: false},
+        min: 'dataMin',
+        max: 'dataMax',
+        axisPointer: {
+          z: 100
+        }
+      }
+    ],
+    yAxis: [
+      {
+        scale: true,
+        splitArea: {
+          show: true
+        }
+      }
+    ],
+    dataZoom: [
+      {
+        type: 'inside',
+        start: 98,
+        end: 100,
+        minValueSpan: 10
+      },
+      {
+        show: true,
+        type: 'slider',
+        bottom: 60,
+        start: 98,
+        end: 100,
+        minValueSpan: 10
+      }
+    ],
+    series: [
+      {
+        name: 'Dow-Jones index',
+        type: 'custom',
+        renderItem: renderItem,
+        dimensions: ['-', 'open', 'close', 'lowest', 'highest'],
+        encode: {
+          x: 0,
+          y: [1, 2, 3, 4],
+          tooltip: [1, 2, 3, 4]
+        },
+        data: values
+      }
+    ]
+  };
+
+  return (
+    <ReactECharts
+      option={option}
+      style={{height: 500}}
+    />
+  );
+};
+
+export default OpenHighLowCloseChart;
+
---
+++ charting-experiments/src/stories/components/OpenHighLowCloseChart.stories.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import OpenHighLowCloseChart from "../../components/OpenHighLowCloseChart";
+import rawData from "../data/stock-DJI";
+
+function splitData(rawData: (string | number)[][]) {
+  const categoryData = [];
+  const values = [];
+  for (let i = 0; i < rawData.length; i++) {
+    categoryData.push(rawData[i][0]);
+    rawData[i][0] = i;
+    values.push(rawData[i]);
+  }
+  return {
+    categoryData: categoryData,
+    values: values
+  };
+}
+
+
+const data = splitData(rawData);
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Charts/Open-High-Low-Close',
+  component: OpenHighLowCloseChart,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    // backgroundColor: {control: 'color'},
+  },
+} as ComponentMeta<typeof OpenHighLowCloseChart>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// @ts-ignore
+const Template: ComponentStory<typeof OpenHighLowCloseChart> = (args) => <OpenHighLowCloseChart {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  categoryData: data.categoryData,
+  values: data.values,
+};
+
