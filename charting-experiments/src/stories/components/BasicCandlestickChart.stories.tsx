c098257f022bcdf3ce373799fe9b2fcd638d8b69
Christopher Bartling <chris.bartling@gmail.com>
2022-03-02 18:53:03 -0600

    Added BasicCandlestickChart

---
+++ charting-experiments/src/stories/components/BasicCandlestickChart.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import BasicCandlestickChart from "../../components/BasicCandlestickChart";
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Charts/Basic Candlestick',
+  component: BasicCandlestickChart,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    // backgroundColor: {control: 'color'},
+  },
+} as ComponentMeta<typeof BasicCandlestickChart>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// @ts-ignore
+const Template: ComponentStory<typeof BasicCandlestickChart> = (args) => <BasicCandlestickChart {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  // primary: true,
+  // label: 'Button',
+};
+
