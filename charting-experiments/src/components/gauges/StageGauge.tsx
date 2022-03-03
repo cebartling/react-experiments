933bc7139fc103e1683ea9e716662b0560dc77c5
Christopher Bartling <chris.bartling@gmail.com>
2022-03-03 11:15:55 -0600

    Adding StageGauge

---
+++ charting-experiments/src/components/gauges/StageGauge.tsx
@@ -0,0 +1,76 @@
+import ReactECharts from 'echarts-for-react';
+
+const StageGauge = () => {
+  const options = {
+    series: [
+      {
+        type: 'gauge',
+        axisLine: {
+          lineStyle: {
+            width: 30,
+            color: [
+              [0.3, '#67e0e3'],
+              [0.7, '#37a2da'],
+              [1, '#fd666d']
+            ]
+          }
+        },
+        pointer: {
+          itemStyle: {
+            color: 'auto'
+          }
+        },
+        axisTick: {
+          distance: -30,
+          length: 8,
+          lineStyle: {
+            color: '#fff',
+            width: 2
+          }
+        },
+        splitLine: {
+          distance: -30,
+          length: 30,
+          lineStyle: {
+            color: '#fff',
+            width: 2
+          }
+        },
+        axisLabel: {
+          color: 'auto',
+          distance: 40,
+          fontSize: 10
+        },
+        detail: {
+          valueAnimation: true,
+          formatter: '{value} km/h',
+          color: 'auto',
+          fontSize: 12,
+        },
+        data: [
+          {
+            value: 70
+          }
+        ]
+      }
+    ]
+  };
+
+  // setInterval(function () {
+  //   myChart.setOption<echarts.EChartsOption>({
+  //     series: [
+  //       {
+  //         data: [
+  //           {
+  //             value: +(Math.random() * 100).toFixed(2)
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   });
+  // }, 2000);
+
+  return (<ReactECharts option={options} />);
+};
+
+export default StageGauge;
---
+++ charting-experiments/src/stories/components/gauges/StageGauge.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import StageGauge from "../../../components/gauges/StageGauge";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Gauges/Stage',
+  component: StageGauge,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    // backgroundColor: {control: 'color'},
+  },
+} as ComponentMeta<typeof StageGauge>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// @ts-ignore
+const Template: ComponentStory<typeof StageGauge> = (args) => <StageGauge {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  // primary: true,
+  // label: 'Button',
+};
+
