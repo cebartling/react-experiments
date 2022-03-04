81b6bfffa27265cb0e4724bb101f1694bf152591
Christopher Bartling <chris.bartling@gmail.com>
2022-03-04 16:22:10 -0600

    Initial revision of the profile form

--- react-hook-form-experiment/src/components/profile-form/ProfileForm.tsx
+++ react-hook-form-experiment/src/components/profile-form/ProfileForm.tsx
@@ -1,4 +1,6 @@
+import classNames from 'classnames';
 import * as React from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 type ProfileFormData = {
@@ -10,32 +12,72 @@ type ProfileFormData = {
 export default function ProfileForm() {
     const {
         register,
-        setValue,
+        // setValue,
         handleSubmit,
         formState: { errors },
     } = useForm<ProfileFormData>();
+    let [isFormSubmitted, setFormSubmitted] = useState<boolean>(false);
 
-    const onSubmit = handleSubmit((data) => console.log(data));
+    const onSubmit = handleSubmit((data) => {
+        setFormSubmitted(true);
+        console.log(data);
+    });
 
     return (
-        <form onSubmit={onSubmit}>
-            <label>First Name</label>
-            <input {...register('firstName')} />
-            <label>Last Name</label>
-            <input {...register('lastName')} />
-            <label>Email address</label>
-            <input {...register('emailAddress')} />
-            {/*<button*/}
-            {/*    type="button"*/}
-            {/*    onClick={() => {*/}
-            {/*        setValue('lastName', 'luo'); // ✅*/}
-            {/*        // setValue("firstName", true); // ❌: true is not string*/}
-            {/*        // errors.bill; // ❌: property bill does not exist*/}
-            {/*    }}*/}
-            {/*>*/}
-            {/*    SetValue*/}
-            {/*</button>*/}
-            <button type="submit">Save</button>
-        </form>
+        <div className="p-2">
+            <form
+                onSubmit={onSubmit}
+                noValidate={true}
+                className={classNames({
+                    'needs-validation': true,
+                    'was-validated': isFormSubmitted,
+                })}
+            >
+                <div className="form-floating mb-3">
+                    <input
+                        {...register('firstName')}
+                        id="firstName "
+                        type="text"
+                        className="form-control"
+                        placeholder="John"
+                        required={true}
+                    />
+                    <label htmlFor="firstName">Given name</label>
+                    <div className="valid-feedback">Looks good!</div>
+                    <div className="invalid-feedback">Given name is a required field!</div>
+                </div>
+                <div className="form-floating mb-3">
+                    <input
+                        {...register('lastName')}
+                        id="lastName"
+                        type="text"
+                        className="form-control"
+                        placeholder="Smith"
+                        required={true}
+                    />
+                    <label htmlFor="lastName">Surname</label>
+                    <div className="valid-feedback">Looks good!</div>
+                    <div className="invalid-feedback">Surname is a required field!</div>
+                </div>
+                <div className="form-floating mb-3">
+                    <input
+                        {...register('emailAddress')}
+                        id="emailAddress"
+                        type="email"
+                        className="form-control"
+                        placeholder="john.smith@mycompany.biz"
+                        required={true}
+                    />
+                    <label htmlFor="emailAddress">Email address</label>
+                    <div className="valid-feedback">Looks good!</div>
+                    <div className="invalid-feedback">Email address is a required field!</div>
+                </div>
+                <div className="mb-3">
+                    <button className="btn btn-primary" type="submit">
+                        Save
+                    </button>
+                </div>
+            </form>
+        </div>
     );
 }
