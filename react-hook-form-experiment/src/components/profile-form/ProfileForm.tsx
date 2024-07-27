7f1a94df063f052b1a651723be334b7cfd391072
Christopher Bartling <chris.bartling@gmail.com>
2024-07-27 16:34:07 -0500

    feat: updating profile form

--- react-hook-form-experiment/src/components/profile-form/ProfileForm.tsx
+++ react-hook-form-experiment/src/components/profile-form/ProfileForm.tsx
@@ -1,93 +1,100 @@
-import classNames from 'classnames';
-import * as React from 'react';
-import { useState } from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
+import classNames from "classnames";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 type ProfileFormData = {
-    firstName: string;
-    lastName: string;
-    emailAddress: string;
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
 };
 
 export default function ProfileForm() {
-    const methods = useForm<ProfileFormData>();
-    let [isFormSubmitted, setFormSubmitted] = useState<boolean>(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm<ProfileFormData>();
+  const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
 
-    const onSubmit = methods.handleSubmit((data) => {
-        setFormSubmitted(true);
-        console.log(data);
-    });
+  const onSubmit = handleSubmit((data) => {
+    setFormSubmitted(true);
+    console.log(data);
+  });
 
-    return (
-        <div className="p-2">
-            <FormProvider {...methods}>
-                <form
-                    onSubmit={onSubmit}
-                    noValidate={true}
-                    className={classNames({
-                        row: true,
-                        'needs-validation': true,
-                        'was-validated': isFormSubmitted,
-                    })}
-                >
-                    <div className="row">
-                        <div className="form-floating mb-3 col-sm-6">
-                            <input
-                                {...methods.register('firstName')}
-                                id="firstName"
-                                type="text"
-                                className="form-control"
-                                placeholder="John"
-                                required={true}
-                            />
-                            <label htmlFor="firstName">Given name</label>
-                        </div>
-                        <div className="valid-feedback col-sm-6">Looks good!</div>
-                        <div className="invalid-feedback col-sm-6">
-                            {methods.formState.errors.firstName}
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="form-floating mb-3 col-sm-6">
-                            <input
-                                {...methods.register('lastName')}
-                                id="lastName"
-                                type="text"
-                                className="form-control"
-                                placeholder="Smith"
-                                required={true}
-                            />
-                            <label htmlFor="lastName">Surname</label>
-                        </div>
-                        <div className="valid-feedback col-sm-6">Looks good!</div>
-                        <div className="invalid-feedback col-sm-6">
-                            {methods.formState.errors.lastName}
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="form-floating mb-3 col-sm-6">
-                            <input
-                                {...methods.register('emailAddress')}
-                                id="emailAddress"
-                                type="email"
-                                className="form-control"
-                                placeholder="john.smith@mycompany.biz"
-                                required={true}
-                            />
-                            <label htmlFor="emailAddress">Email address</label>
-                        </div>
-                        <div className="valid-feedback col-sm-6">Looks good!</div>
-                        <div className="invalid-feedback col-sm-6">
-                            {methods.formState.errors.emailAddress}
-                        </div>
-                    </div>
-                    <div className="mb-3">
-                        <button className="btn btn-primary" type="submit">
-                            Save
-                        </button>
-                    </div>
-                </form>
-            </FormProvider>
+
+  console.log(watch("firstName"));
+  console.log(watch("lastName"));
+  console.log(watch("emailAddress"));
+
+  return (
+    <div className="p-2">
+      <form
+        onSubmit={onSubmit}
+        noValidate={true}
+        className={classNames({
+          row: true,
+          "needs-validation": true,
+          "was-validated": formSubmitted
+        })}
+      >
+        <div className="row">
+          <div className="form-floating mb-3 col-sm-6">
+            <input
+              {...register("firstName")}
+              id="firstName"
+              type="text"
+              className="form-control"
+              placeholder="John"
+              required={true}
+            />
+            <label htmlFor="firstName">Given name</label>
+          </div>
+          <div className="valid-feedback col-sm-6">Looks good!</div>
+          <div className="invalid-feedback col-sm-6">
+            {errors.firstName ? errors.firstName.message : null}
+          </div>
         </div>
-    );
+        <div className="row">
+          <div className="form-floating mb-3 col-sm-6">
+            <input
+              {...register("lastName")}
+              id="lastName"
+              type="text"
+              className="form-control"
+              placeholder="Smith"
+              required={true}
+            />
+            <label htmlFor="lastName">Surname</label>
+          </div>
+          <div className="valid-feedback col-sm-6">Looks good!</div>
+          <div className="invalid-feedback col-sm-6">
+            {errors.lastName ? errors.lastName.message : null}
+          </div>
+        </div>
+        <div className="row">
+          <div className="form-floating mb-3 col-sm-6">
+            <input
+              {...register("emailAddress")}
+              id="emailAddress"
+              type="email"
+              className="form-control"
+              placeholder="john.smith@mycompany.biz"
+              required={true}
+            />
+            <label htmlFor="emailAddress">Email address</label>
+          </div>
+          <div className="valid-feedback col-sm-6">Looks good!</div>
+          <div className="invalid-feedback col-sm-6">
+            {errors.emailAddress ? errors.emailAddress.message : null}
+          </div>
+        </div>
+        <div className="mb-3">
+          <button className="btn btn-primary" type="submit">
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
