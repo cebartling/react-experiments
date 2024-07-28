560d3bd6cdc2cf0ae58cde54f7b2dbf7ce813309
Christopher Bartling <chris.bartling@gmail.com>
2024-07-27 21:06:28 -0500

    feat: creating Storybook story for ProfileForm

---
+++ react-hook-form-experiment/src/components/profile-form/ProfileForm.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+import ProfileForm from './ProfileForm';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Forms/ProfileForm',
+    component: ProfileForm,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ProfileForm>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ProfileForm> = (args) => <ProfileForm {...args} />;
+
+// export const Primary = Template.bind({});
+// // More on args: https://storybook.js.org/docs/react/writing-stories/args
+// Primary.args = {
+//     primary: true,
+//     label: 'Button',
+// };
