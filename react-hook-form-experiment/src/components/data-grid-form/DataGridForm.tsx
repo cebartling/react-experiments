c7abb3d9785a8e1b1493d614ac56dd76c2a4ea1f
Christopher Bartling <chris.bartling@gmail.com>
2024-07-29 23:11:26 -0500

    feat: creating DataGridForm component

---
+++ react-hook-form-experiment/src/components/data-grid-form/DataGridForm.tsx
@@ -0,0 +1,39 @@
+import { useForm } from 'react-hook-form';
+
+import cn from '../../util/cn';
+
+type DataGridFormData = {
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+};
+
+export default function DataGridForm() {
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors, isSubmitted },
+  } = useForm<DataGridFormData>({
+    // resolver: zodResolver(schema),
+    reValidateMode: 'onChange',
+    shouldFocusError: true,
+  });
+
+  const onSubmit = handleSubmit((data: DataGridFormData) => {
+    console.log(data);
+  });
+
+  return (
+    <div className="w-full max-w-sm">
+      <form
+        onSubmit={onSubmit}
+        noValidate={true}
+        className={`bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ${cn({
+          'needs-validation': true,
+          'was-validated': isSubmitted,
+        })}`}
+      ></form>
+    </div>
+  );
+}
