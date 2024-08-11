5ce93292f694d32a1d6d592e2e98bd2e8171d80d
Christopher Bartling <chris.bartling@gmail.com>
2024-08-10 21:01:07 -0500

    chore: checkpointing WIP

--- mui-data-grid-react-hook-form/src/features/DemographicsCard.tsx
+++ mui-data-grid-react-hook-form/src/features/DemographicsCard.tsx
@@ -1,12 +1,24 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useFieldArray, useForm } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import {
+  DataGrid,
+  GridColDef,
+  GridEventListener,
+  GridRowEditStopReasons,
+  GridRowModel,
+  GridRowModes,
+  GridRowModesModel,
+  GridRowsProp,
+  GridSlots,
+  GridToolbarContainer,
+} from '@mui/x-data-grid';
 import { Button } from '@mui/material';
 import { createGridColDefArray } from './createGridColDefArray.tsx';
 import { DemographicsFormData } from '../types';
+import AddIcon from '@mui/icons-material/Add';
 
-const rows: DemographicsFormData[] = [
+const initialRows: DemographicsFormData[] = [
   {
     id: 1,
     firstName: 'Kristoffer',
@@ -27,22 +39,108 @@ const rows: DemographicsFormData[] = [
   } as DemographicsFormData,
 ];
 
+interface EditToolbarProps {
+  oldRows: GridRowsProp;
+  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+  setRowModesModel: (
+    newModel: (oldModel: GridRowModesModel) => GridRowModesModel,
+  ) => void;
+}
+
+function EditToolbar({ oldRows, setRows, setRowModesModel }: EditToolbarProps) {
+
+  const handleClick = () => {
+    const id = oldRows.length + 1;
+    setRows((oldRows) => [...oldRows, { id, firstName: '', lastName: '', email: '' }]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: 'firstName' },
+    }));
+  };
+
+  return (
+    <GridToolbarContainer>
+      <Button color="primary"
+              startIcon={<AddIcon />}
+              onClick={handleClick}>
+        Add
+      </Button>
+    </GridToolbarContainer>
+  );
+}
+
+
 export function DemographicsCard() {
+  const [rows, setRows] = useState(initialRows);
+  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({});
   const { handleSubmit, control, register } = useForm();
   useFieldArray({
     control,
     name: 'form',
   });
-
-
   const columns: GridColDef[] = useMemo(() => createGridColDefArray(control, register), [control, register]);
+
+  const handleRowEditStop: GridEventListener<'rowEditStop'> = (params, event) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const processRowUpdate = (newRow: GridRowModel<DemographicsFormData>) => {
+    const updatedRow = { ...newRow };
+    const newRows = rows.map((row) => (row.id === newRow.id ? updatedRow : row));
+    setRows(newRows);
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  // const handleEditClick = (id: GridRowId) => () => {
+  //   setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  // };
+
+  // const handleSaveClick = (id: GridRowId) => () => {
+  //   setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  // };
+  //
+  // const handleDeleteClick = (id: GridRowId) => () => {
+  //   setRows(rows.filter((row) => row.id !== id));
+  // };
+  //
+  // const handleCancelClick = (id: GridRowId) => () => {
+  //   setRowModesModel({
+  //     ...rowModesModel,
+  //     [id]: { mode: GridRowModes.View, ignoreModifications: true },
+  //   });
+
+  //   const editedRow = rows.find((row) => row.id === id);
+  //   if (editedRow!.isNew) {
+  //     setRows(rows.filter((row) => row.id !== id));
+  //   }
+  // };
+
 
   return (
     <div className="p-5">
       <h2>Demographics</h2>
       <form onSubmit={handleSubmit((data) => console.log(data))}>
         <div className="w-full h-[500px]">
-          <DataGrid rows={rows} columns={columns} editMode="row" />
+          <DataGrid rows={rows}
+                    columns={columns}
+                    editMode="row"
+                    rowModesModel={rowModesModel}
+                    onRowModesModelChange={handleRowModesModelChange}
+                    onRowEditStop={handleRowEditStop}
+                    processRowUpdate={processRowUpdate}
+                    slots={{
+                      toolbar: EditToolbar as GridSlots['toolbar'],
+                    }}
+                    slotProps={{
+                      toolbar: { oldRows: rows, setRows, setRowModesModel },
+                    }}
+          />
         </div>
         <div className="mt-4">
           <Button type="submit" variant="contained">
