788f7760b7368454f12b55a78f5d131354468059
Christopher Bartling <chris.bartling@gmail.com>
2024-12-16 13:00:55 -0600

    feat: migrating to styled-components impl

--- image-with-fallback/src/components/FallbackSkeleton.tsx
+++ image-with-fallback/src/components/FallbackSkeleton.tsx
@@ -1,19 +1,19 @@
+import styled from 'styled-components';
+
+const StyledDiv = styled.div<{ width: number; height: number }>`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  background-color: #e0e0e0;
+  border-radius: 4px;
+  animation: pulse 1.5s infinite;
+`;
+
 const FallbackSkeleton = ({
   width = 300,
   height = 200,
 }: {
   width?: number;
   height?: number;
-}) => (
-  <div
-    style={{
-      width: `${width}px`,
-      height: `${height}px`,
-      backgroundColor: '#e0e0e0',
-      borderRadius: '4px',
-      animation: 'pulse 1.5s infinite',
-    }}
-  ></div>
-);
+}) => <StyledDiv width={width} height={height}></StyledDiv>;
 
 export default FallbackSkeleton;
