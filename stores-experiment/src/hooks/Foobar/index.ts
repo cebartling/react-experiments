dbc37ba5c057b4e122bc37d77a6eef1a207de3d5
Christopher Bartling <chris.bartling@gmail.com>
2025-03-21 18:44:41 -0500

    feat: adding zustand and zod deps

---
+++ stores-experiment/src/hooks/Foobar/index.ts
@@ -0,0 +1,29 @@
+import { useSyncExternalStore } from 'react';
+import create from 'zustand/vanilla';
+
+// Define the state shape
+interface FoobarState {
+  foo: string;
+  bar: number;
+  setFoo: (value: string) => void;
+  setBar: (value: number) => void;
+}
+
+// Create a vanilla Zustand store (outside of React context)
+export const foobarStore = create<FoobarState>((set) => ({
+  foo: 'hello',
+  bar: 42,
+  setFoo: (value) => set({ foo: value }),
+  setBar: (value) => set({ bar: value }),
+}));
+
+// Create a snapshot getter
+const getSnapshot = () => foobarStore.getState();
+
+// Create a subscribe function for useSyncExternalStore
+const subscribe = (callback: () => void) => foobarStore.subscribe(callback);
+
+// Custom hook that provides the full state
+export function useFoobar(): FoobarState {
+  return useSyncExternalStore(subscribe, getSnapshot);
+}
