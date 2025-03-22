a790f2be77e734872652ab4032629213c69bded0
Christopher Bartling <chris.bartling@gmail.com>
2025-03-21 22:56:05 -0500

    chore: fixing eslint issues

--- stores-experiment/src/hooks/Foobar/localforageStorage.ts
+++ stores-experiment/src/hooks/Foobar/localforageStorage.ts
@@ -2,6 +2,7 @@ import localforage from 'localforage';
 import {StateStorage} from "zustand/middleware";
 
 export const localforageStorage = (key: string): StateStorage => ({
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     getItem: async (_key: string): Promise<string | null> => {
         const value = await localforage.getItem<string>(key);
         return value ?? null;
@@ -9,6 +10,7 @@ export const localforageStorage = (key: string): StateStorage => ({
     setItem: async (_key: string, value: string): Promise<void> => {
         await localforage.setItem(key, value);
     },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     removeItem: async (_key: string): Promise<void> => {
         await localforage.removeItem(key);
     },
