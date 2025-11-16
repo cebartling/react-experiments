5b1a5b401a8a7fd393970c9a1ce034a0a55e1a30
Christopher Bartling <chris.bartling@gmail.com>
2025-11-15 22:29:40 -0600

    feat: showing satellite base layer

--- maplibre-gl-experiment/src/components/Map.tsx
+++ maplibre-gl-experiment/src/components/Map.tsx
@@ -1,6 +1,30 @@
 import { Map as MapGL, NavigationControl } from '@vis.gl/react-maplibre'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
+// Satellite style using free EOX Sentinel-2 cloudless imagery
+const satelliteStyle = {
+  version: 8 as const,
+  sources: {
+    'satellite': {
+      type: 'raster' as const,
+      tiles: [
+        'https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2020_3857/default/g/{z}/{y}/{x}.jpg'
+      ],
+      tileSize: 256,
+      attribution: '© <a href="https://s2maps.eu" target="_blank">Sentinel-2 cloudless</a> by <a href="https://eox.at/" target="_blank">EOX IT Services GmbH</a>'
+    }
+  },
+  layers: [
+    {
+      id: 'satellite',
+      type: 'raster' as const,
+      source: 'satellite',
+      minzoom: 0,
+      maxzoom: 22
+    }
+  ]
+}
+
 function Map() {
   return (
     <div className="p-8">
@@ -12,7 +36,7 @@ function Map() {
             latitude: 44.7975,
             zoom: 12
           }}
-          mapStyle="https://demotiles.maplibre.org/style.json"
+          mapStyle={satelliteStyle}
           style={{ width: '100%', height: '100%' }}
         >
           <NavigationControl position="top-right" />
